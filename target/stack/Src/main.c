7fcfbd96b6d549483e3715815eed95479bb09770
Athul Muralidhar <athul.m68@gmail.com>
2024-08-06 20:03:43 +0200

    added stack exercise

--- target/stack/Src/main.c
+++ target/stack/Src/main.c
@@ -17,6 +17,7 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -29,12 +30,16 @@
 
 #define STACK_START  (SRAM_END)
 #define STACK_MSP_START  (STACK_START)
-#define STACK_MSP_END  ((STACK_MSP_START) + 512)
-#define STACK_START  (SRAM_END)
+#define STACK_MSP_END  ((STACK_MSP_START) - 512)
+#define STACK_PSP_START  (STACK_MSP_END)
 
+void generate_exception();
+void __attribute__((naked)) change_stack_pointer_to_process_stack_pointer();
 
 int main(void)
 {
+	change_stack_pointer_to_process_stack_pointer();
+
 
 	int result;
 
@@ -42,9 +47,36 @@ int main(void)
 
 	printf("result: %d\n", result);
 
+	generate_exception();
+
 
     /* Loop forever */
 	for(;;);
+}
+
+void generate_exception() {
+	__asm volatile("SVC #0x2");
+}
+
+
+void SVC_Handler() {
+	printf("fake exception in svc handler");
+}
+
+
+void __attribute__((naked)) change_stack_pointer_to_process_stack_pointer() {
+	// assembly macros
+	__asm volatile(".equ SRAM_END, (0x20000000 + (128*1024))");
+	__asm volatile(".equ STACK_PSP_START, (SRAM_END - 512)");
+
+	// Initialize process_stack_pointer
+	__asm volatile("LDR R0,=STACK_PSP_START");
+	// copy r0 to process_stack_pointer
+	__asm volatile("MSR PSP,R0");
+	// change stack pointer to process_stack_pointer
+
+	__asm volatile("MOV R0,0x02");   // the 1st bit when set to 1 sets the stack pointer to process_stack_pointer
+	__asm volatile("MSR CONTROL,R0");
 }
 
 int add(int a, int b) {
