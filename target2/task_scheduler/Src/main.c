e965cdab42aa95fab12a5d491c86e352fe289edf
Athul Muralidhar <athul.m68@gmail.com>
2024-09-28 12:24:27 +0530

    only systick handler left

--- target2/task_scheduler/Src/main.c
+++ target2/task_scheduler/Src/main.c
@@ -18,48 +18,132 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include"main.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
 #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-// stack sizes
-#define SIZE_TASK_STACK  1024U
-#define SIZE_SCHEDULER_STACK  1024U
-// SRAM
-#define SRAM_START 0x20000000U
-#define SIZE_SRAM  ((128) * (1024))
-#define SRAM_END  ((SRAM_START) + SIZE_SRAM)
-// tasks
-#define T1_STACK_START SRAM_END
-#define T2_STACK_START ((SRAM_END) - (SIZE_TASK_STACK))
-#define T3_STACK_START ((SRAM_END) - (2 * SIZE_TASK_STACK))
-#define T4_STACK_START ((SRAM_END) - (3 * SIZE_TASK_STACK))
-// scheduler
-#define SCHEDULER_STACK_START ((SRAM_END) - (SIZE_TASK_STACK))
-
-// desired exception frequency
-#define EXCEPTION_FREQUENCY 1000U
-// high speed internal clock - the value is given in the st32f4xxx reference manual
-#define HSI_CLOCK  16000000U
-#define SYSTICK_TIMER_CLOCK HSI_CLOCK
-
-// system tick timer
+// system tick timer initialization
 void init_systick_timer(uint32_t freq);
+//scheduler stack initialization
+__attribute__((naked)) void init_scheduler_stack(uint32_t top_of_stack);
 // tasks
 void task1_handler();
 void task2_handler();
 void task3_handler();
 void task4_handler();
+// enable all faults
+void enable_processor_faults();
+// naked function to switch from MSP to PSP
+__attribute__ ((naked)) void switch_sp_to_psp();
+// Initialize tasks to dummy variables
+void init_task_stacks();
 
+uint32_t PSP_of_tasks[MAX_TASKS] = {
+T1_STACK_START,
+T2_STACK_START,
+T3_STACK_START,
+T4_STACK_START };
+
+uint32_t task_handlers[MAX_TASKS];
+
+uint8_t current_task = 0;  // task 1 is running
 
 int main(void) {
+
+	enable_processor_faults();
+
+	init_scheduler_stack(SCHEDULER_STACK_START);
+
+	task_handlers[0] = (uint32_t) task1_handler;
+	task_handlers[1] = (uint32_t) task2_handler;
+	task_handlers[2] = (uint32_t) task3_handler;
+	task_handlers[3] = (uint32_t) task4_handler;
+
+	init_task_stacks();
+
 	init_systick_timer(EXCEPTION_FREQUENCY);
+
+
+	switch_sp_to_psp(); // we have to switch so that we can run the task in thread mode
+
+	task1_handler();
+
+
 	/* Loop forever */
-	for (;;){};
+	for (;;) {
+	};
 }
 
-void init_systick_timer(uint32_t freq){
+uint32_t get_psp_value() {
+	return PSP_of_tasks[current_task];
+}
+
+__attribute__ ((naked)) void switch_sp_to_psp() {
+	// 1. Initialize PSP to task 1 stack start - as this is the first task we want to run
+	__asm volatile ("PUSH {LR}"); // save LR value for later so that we can return back to main function
+	// get the value of PSP of the current task
+	__asm volatile ("BL get_psp_value");  // the uint32 return value is stored in r0 which we can use
+	__asm volatile ("MSR PSP,R0");  // Initialize PSP to point to our current task
+	__asm volatile ("POP {LR}"); // return LR to point to main function
+
+	// 2. change from SP to PSP using the CONTROL register
+	__asm volatile ("MOV R0,#0x02");  // setting the 2nd bit in r0
+	// this writes r0 to CONTROL register, thereby setting the 2nd bit to one and
+	// changing execution from MSP to PSP
+	__asm volatile ("MSR CONTROL, R0");
+
+	// 3. return to main
+	__asm volatile ("BX LR");
+
+}
+
+void enable_processor_faults() {
+	uint32_t *pSHCSR_ADDRESS = (uint32_t*) 0xE000ED24; // System Handler Control and State Register
+
+	*pSHCSR_ADDRESS |= (1 << 16); // UsageFault enable bit
+	*pSHCSR_ADDRESS |= (1 << 17); // BusFault enable bit,
+	*pSHCSR_ADDRESS |= (1 << 18); // MemManage enable bit,
+
+}
+
+void init_task_stacks() {
+	uint32_t *pPSP;
+
+	for (int i = 0; i < MAX_TASKS; i++) {
+		pPSP = (uint32_t*) PSP_of_tasks[i]; // gets the current task's stack start
+
+		// XPSR
+		pPSP--; // full descending stack
+		*pPSP = DUMMY_XPSR;
+
+		// PC
+		pPSP--;
+		*pPSP = RETURN_TO_THREAD_MODE;
+
+		// for GPIO registers r0 to r12
+		for (int j = 0; j < 13; j++) {
+			pPSP--;
+			*pPSP = 0;
+		}
+
+		// NOTE: we need to save the value back to the PSP_of_tasks
+		// only then does the processor pop all the states for us
+		PSP_of_tasks[i] = (uint32_t) pPSP;
+
+	}
+
+}
+
+// naked function as MSP is a special register and it is not accessible through C code
+__attribute__((naked)) void init_scheduler_stack(uint32_t top_of_stack) {
+	__asm volatile("MSR MSP,%0"::"r" (top_of_stack) :);
+	__asm volatile("BX LR");
+	// return to the caller
+}
+
+void init_systick_timer(uint32_t freq) {
 	// systick reload value register: 0xE000E014
 	/*
 	 * - the systick reload value register's (SVR) value is copied into the current value register CVR
@@ -72,22 +156,20 @@ void init_systick_timer(uint32_t freq){
 	// systick control and status register (SCSR): 0xE000E010
 	uint32_t *pSystick_Control_And_Status_Register = (uint32_t*) 0xE000E010;
 
-	uint32_t count_value = SYSTICK_TIMER_CLOCK /  freq - 1;
-
+	uint32_t count_value = SYSTICK_TIMER_CLOCK / freq - 1;
 
 	// clear the value in the SVR
-	*pSystick_Reload_Value_Register &= ~(0x00FFFFFFFF);   // only 0 to 24 bits are valid in this register
+	*pSystick_Reload_Value_Register &= ~(0x00FFFFFFFF); // only 0 to 24 bits are valid in this register
 
 	// load value
 	*pSystick_Reload_Value_Register |= count_value;
 
 	// settings for the SCSR
-	*pSystick_Control_And_Status_Register  |= (1 << 1);   // Enables SysTick exception request
-	*pSystick_Control_And_Status_Register  |= (1 << 2);   // sets the clock source to processor clock
+	*pSystick_Control_And_Status_Register |= (1 << 1); // Enables SysTick exception request
+	*pSystick_Control_And_Status_Register |= (1 << 2); // sets the clock source to processor clock
 
 	// enables the systick
 	*pSystick_Control_And_Status_Register |= (1 << 0);
-
 
 }
 
@@ -117,4 +199,21 @@ void task4_handler() {
 	while (1) {
 		printf("this is task4");
 	}
+}
+
+void HardFault_Handler() {
+	printf("exception in hard fault\n");
+	while (1) {
+	};
+}
+
+void MemManage_Handler() {
+	printf("exception in MemManage_Handler\n");
+	while (1) {
+	};
+}
+void BusFault_Handler() {
+	printf("exception in BusFault_Handlern");
+	while (1) {
+	};
 }
